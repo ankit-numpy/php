79efc98a10f054ebd4912026dab3ec1760d07767
Ankit Dixit <88057272+ankit-numpy@users.noreply.github.com>
2024-03-13 10:36:08 +0530

    Add files via upload

---
+++ components/footer.php
@@ -0,0 +1,36 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box" style="background-color: #c0b5dd;">
+         <img src="images/email-icon.png" alt="">
+         <h3>our email</h3>
+         <a href="mailto:sweet_tooth@gmail.com">sweet_tooth@gmail.com</a>
+      </div>
+
+      <div class="box" style="background-color: #c0b5dd;">
+         <img src="images/clock-icon.png" alt="">
+         <h3>opening hours</h3>
+         <p>07:00am to 01:00pm</p>
+      </div>
+
+      <div class="box" style="background-color: #c0b5dd;">
+         <img src="images/map-icon.png" alt="">
+         <h3>our address</h3>
+         <a href="https://www.google.com/maps?ll=38.905925,-77.045752&z=15&t=m&hl=en&gl=IN&mapclient=embed&q=2025+M+St+NW+Washington,+DC+20036+USA">2025 M Street, NYC, USA </a>
+      </div>
+
+      <div class="box" style="background-color: #c0b5dd;">
+         <img src="images/phone-icon.png" alt="">
+         <h3>our number</h3>
+         <a href="tel:1234567890">987-30-25515</a>
+      </div>
+
+   </section>
+
+
+</footer>
+
+<div class="loader">
+   <img src="images/loader.gif" alt="">
+</div>
