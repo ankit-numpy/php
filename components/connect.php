8d5e368fcd0cc66abb9c23c50d1509fd3e94611c
Ankit Dixit <88057272+ankit-numpy@users.noreply.github.com>
2024-03-13 11:07:40 +0530

    Update connect.php

--- components/connect.php
+++ components/connect.php
@@ -1,9 +1,12 @@
 <?php
 
-$db_name = 'mysql:host=localhost;dbname=food_db';
-$user_name = 'root';
-$user_password = '';
+$db_name = 'mysql:host=hrishimysqlserver.mysql.database.azure.com:3306;dbname=food_db';
+$user_name = 'hrishi@hrishimysqlserver';
+$user_password = 'YourStrongPassword!';
 
 $conn = new PDO($db_name, $user_name, $user_password);
 
 ?>
+
+
+String jdbcURL = "jdbc:mysql://hrishimysqlserver.mysql.database.azure.com:3306/food_db";
