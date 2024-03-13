0c958a8b87e4f0c1e2e054bac0c008ce0f18c315
Ankit Dixit <88057272+ankit-numpy@users.noreply.github.com>
2024-03-13 10:26:50 +0530

    Update home.php

--- home.php
+++ home.php
@@ -1,4 +1,18 @@
+<?php
 
+include 'components/connect.php';
+
+session_start();
+
+if(isset($_SESSION['user_id'])){
+   $user_id = $_SESSION['user_id'];
+}else{
+   $user_id = '';
+};
+
+include 'components/add_cart.php';
+
+?>
 
 <!DOCTYPE html>
 <html lang="en">
